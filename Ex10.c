4d18562b63f23e4b13c42f0398cbdfce1d1d6f9b
libanesd <44514484+libanesd@users.noreply.github.com>
2019-05-27 20:49:10 -0300

    Create Ex10.c

---
+++ Ex10.c
@@ -0,0 +1,41 @@
+/*
+ Leonardo H. Zeaim
+ */
+/*
+ Crie quatro funções, somarDoisNumeros,
+ subtrairDoisNumeros, dividirDoisNumeros,
+ multiplicarDoisNumeros, usando passagem de parâmetros
+ por referência. Crie um programa que utilize todas as
+ funções e mostre o resultado em tela.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int somarDoisNumeros(int* x, int* y){
+    printf("%d + %d = %d\n",*x,*y,*x + *y);
+}
+int substiruirDoisNumeros(int* x,int* y){
+    printf("%d - %d = %d\n",*x,*y,*x - *y);
+}
+int dividirDoisNumeros(int* x,int* y){
+    printf("%d / %d = %d\n",*x,*y,*x / *y);
+}
+int multiplicarDoisNumeros(int* x,int* y){
+    printf("%d x %d = %d\n",*x,*y,*x * *y);
+}
+
+int main(void) {
+    int a,b;
+
+    printf("digite dois numeros\n");
+    scanf("%d %d",&a,&b);
+
+    somarDoisNumeros(&a,&b);
+    substiruirDoisNumeros(&a,&b);
+    dividirDoisNumeros(&a,&b);
+    multiplicarDoisNumeros(&a,&b);
+
+
+    return 0;
+}
